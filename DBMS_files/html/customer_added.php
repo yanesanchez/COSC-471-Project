9b59cd38477ee654051fa210930417f986a7cd85
bstobby <bstobby@wccnet.edu>
2021-11-03 19:09:48 -0400

    updated user tables

--- DBMS_files/html/customer_added.php
+++ DBMS_files/html/customer_added.php
@@ -95,35 +95,51 @@ if(isset($_POST['register_submit'])){
 
         require_once('../PDO_connect.php');
 
-		$stmt = $pdo -> prepare("INSERT INTO customer (username, pin, first_name, last_name, 
+        $stmt1 = $pdo -> prepare("INSERT INTO USER (type) VALUE (:type)");
+        $type = 'R';
+        $stmt1->bindParam(':type', $type);
+        $stmt1->execute();
+
+		$stmt2 = $pdo -> prepare("INSERT INTO REGISTERED_USER (id, username, pin, first_name, last_name, 
         address, city, state, zip, credit_card, card_number, expiration) 
-        VALUES (:username, :pin, :first_name, :last_name, 
+        VALUES (LAST_INSERT_ID(), :username, :pin, :first_name, :last_name, 
         :address, :city, :state, :zip, :credit_card, :card_number, :expiration)");
 
-		$stmt->bindParam(':username', $username);
-		$stmt->bindParam(':pin', $pin);
-		$stmt->bindParam(':first_name', $firstname);
-		$stmt->bindParam(':last_name', $lastname);
-		$stmt->bindParam(':address', $address);
-		$stmt->bindParam(':city', $city);
-		$stmt->bindParam(':state', $state);
-		$stmt->bindParam(':zip', $zip);
-		$stmt->bindParam(':credit_card', $credit_card);
-		$stmt->bindParam(':card_number', $card_number);
-		$stmt->bindParam(':expiration', $expiration);
+		$stmt2->bindParam(':username', $username);
+		$stmt2->bindParam(':pin', $pin);
+		$stmt2->bindParam(':first_name', $firstname);
+		$stmt2->bindParam(':last_name', $lastname);
+		$stmt2->bindParam(':address', $address);
+		$stmt2->bindParam(':city', $city);
+		$stmt2->bindParam(':state', $state);
+		$stmt2->bindParam(':zip', $zip);
+		$stmt2->bindParam(':credit_card', $credit_card);
+		$stmt2->bindParam(':card_number', $card_number);
+		$stmt2->bindParam(':expiration', $expiration);
 
-		$stmt->execute();
+		$stmt2->execute();
+
+        $stmt3 = $pdo -> prepare("SELECT LAST_INSERT_ID() as id");
+        $stmt3 -> execute();
+
+        $cart_user_id = $stmt3->fetch();
+
+        print_r($cart_user_id);
+
+        $stmt4 = $pdo -> prepare("INSERT INTO SHOPPING_CART (user_id) VALUES (:user_id)" );
+        $stmt4->bindParam(':user_id', $cart_user_id['id']);
+        $stmt4->execute();
 
       //  mysqli_stmt_bind_param($stmt, "sisssssssss", $username, $pin, $firstname, 
       //  $lastname, $address, $city, $state, $zip, $credit_card, $card_number, $expiration);
 
-       // mysqli_stmt_bind_param($stmt);
+      // mysqli_stmt_bind_param($stmt);
 
-             $affected_rows = $stmt->rowCount();
+             $affected_rows = $stmt1->rowCount();
 
         if($affected_rows == 1){
-            echo 'Customer added';
-
+            header("Location: screen2.php"); 
+            exit;
             $stmt=null;
 			$pdo=null;
 
@@ -135,13 +151,12 @@ if(isset($_POST['register_submit'])){
             $stmt=null;
 			$pdo=null;
         }
- //       else {
+   //         else {
    //         echo 'enter the following data <br />';
-
    //         foreach($data_missing as $missing){
-    //            echo "$missing<br />";
-          //  }
-	//	}
+   //         echo "$missing<br />";
+   //              }
+   //	}
 
     }
 }
