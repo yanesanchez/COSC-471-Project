865e1c9e9e3fb506bef6f274754ef87cdd587243
yanesanchez <yaneli.dev@gmail.com>
2021-12-03 22:38:01 -0500

    basic structure of admin reports

--- DBMS_files/html/reports.php
+++ DBMS_files/html/reports.php
@@ -11,24 +11,65 @@ print_r($_SESSION);
 <html>
 <head>
 	<title> REPORTS </title>
+	<style>
+		table.main { align: center; border:1px solid blue;" }
+	</style>
 </head>
 <body>
+	<h2> ADMINISTRATOR REPORTS </h2>
+	<!-- TOTAL NUMBER of REGISTERED CUSTOMERS in the system AT THE TIME AND DATE OF INQUIRY  -->
+	<table>
+		<tr>
+			<th colspan="2"> Total Registered Customers </th>
+		</tr>
+		<tr>
+			<?php
+				// query for number of users
+					//<td>  </td>
+				// time and date of inquiry
+					//<td>  </td>
+			?>
+		</tr>
+	</table>
 
-	<!-- Total number of registered customers in the system at the time and date of inquiry  -->
-	<?php
 
-	?>
+	<!-- TOTAL NUMBER of BOOK TITLES available in EACH CATEGORY -->
+	<table>
+		<tr>
+			<th colspan="2"> Book Titles & Categories Available </th>
+		</tr>
+		<tr>
+			<?php
+				
+			?>
+		</tr>
+	</table>
 
-	<!-- Total number of book titles available in each category -->
+
+	<!-- AVERAGE MONTHLY SALES, in dollars, for the CURRENT YEAR, ordered BY MONTH -->
+	<table>
+		<tr>
+			<th colspan="2"> Average Monthly Sales </th>
+		</tr>
+		<tr>
+			<?php
+				
+			?>
+		</tr>
+	</table>
 
 
-
-	<!-- Average monthly sales, in dollars, for the current year, ordered by month -->
-
-
-
-	<!-- All book titles and the number of reviews for each book -->
-
+	<!-- ALL BOOK TITLES and the NUMBER OF REVIEWS for EACH BOOK -->
+	<table>
+		<tr>
+			<th colspan="2"> All Book Titles and Reviews </th>
+		</tr>
+		<tr>
+			<?php
+				
+			?>
+		</tr>
+	</table>
 
 </body>
 </html>
