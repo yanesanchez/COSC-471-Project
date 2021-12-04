8d299b8c41e6aed0c01a89a2dd8ec5e895557627
bstobby <bstobby@wccnet.edu>
2021-12-03 21:48:33 -0500

    updated reset

--- DBMS_files/html/reset.php
+++ DBMS_files/html/reset.php
@@ -5,12 +5,4 @@ session_start();
 session_destroy();
 header("Location: screen2.php"); 
 exit;
-
-//$_SESSION['user_id'] = '';
-//$_SESSION['temp_id'] = '';
-//$_SESSION['cart_id'] = false;
-//$_SESSION['valid'] = false;
-
-
-
 ?>
